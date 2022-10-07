1a8a1eaffcc2754801114be545ebedaecabf63ae
David Hong <dahong67@wharton.upenn.edu>
2022-10-06 23:21:08 -0700

    Fix gcp test, add test for default loss
    Incorrectly compared `X` with `M` instead of `Mh`.

--- test/gcp-opt.jl
+++ test/gcp-opt.jl
@@ -1,16 +1,19 @@
 ## GCP decomposition - full optimization
 
-Random.seed!(0)
 @testset "least squares" begin
-    @testset "size(X)=$sz, rank(X)=$r" for sz in [(3, 4, 5), (30, 40, 50)], r in 1:2
+    @testset "size(X)=$sz, rank(X)=$r" for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
+        Random.seed!(0)
         M = CPD(ones(r), rand.(sz, r))
         X = [M[I] for I in CartesianIndices(size(M))]
         Mh = gcp(X, r, (x, m) -> (x - m)^2, (x, m) -> 2 * (m - x))
-        @test maximum(I -> abs(M[I] - X[I]), CartesianIndices(X)) <= 1e-6
+        @test maximum(I -> abs(Mh[I] - X[I]), CartesianIndices(X)) <= 1e-5
 
         Xm = convert(Array{Union{Missing,eltype(X)}}, X)
         Xm[1, 1, 1] = missing
         Mm = gcp(Xm, r, (x, m) -> (x - m)^2, (x, m) -> 2 * (m - x))
-        @test maximum(I -> abs(Mm[I] - X[I]), CartesianIndices(X)) <= 1e-4
+        @test maximum(I -> abs(Mm[I] - X[I]), CartesianIndices(X)) <= 1e-5
+
+        Mh = gcp(X, r) # test default (least-squares) loss
+        @test maximum(I -> abs(Mh[I] - X[I]), CartesianIndices(X)) <= 1e-5
     end
 end
