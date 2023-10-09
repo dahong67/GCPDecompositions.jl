af1010c2213c3b064c904cf6f91ebd3d31dfb837
David Hong <hong@udel.edu>
2023-10-09 16:49:18 -0400

    Bug fixes

--- ext/LossFunctionsExt.jl
+++ ext/LossFunctionsExt.jl
@@ -1,9 +1,9 @@
-module LossFunctinonsExt
+module LossFunctionsExt
 
 using GCPDecompositions
 using LossFunctions
 
-gcp(X::Array, r, loss::LossFunctions.DistanceLoss, lower=-Inf) =
-    _gcp(X, r, (x, m) -> loss(x, m), (x, m) -> LossFunctions.deriv(loss, m, x), lower, (;))
+GCPDecompositions.gcp(X::Array, r, loss::LossFunctions.DistanceLoss, lower=-Inf) =
+    GCPDecompositions._gcp(X, r, (x, m) -> loss(x, m), (x, m) -> LossFunctions.deriv(loss, m, x), lower, (;))
 
 end
