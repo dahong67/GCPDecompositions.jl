243c9d6ed7954773b1a7647e063281703254926e
David Hong <hong@udel.edu>
2023-10-09 15:17:20 -0400

    Fix precompile error

--- ext/LossFunctionsExt.jl
+++ ext/LossFunctionsExt.jl
@@ -1,6 +1,7 @@
 module LossFunctinonsExt
 
 using GCPDecompositions
+using LossFunctions
 
 gcp(X::Array, r, loss::LossFunctions.DistanceLoss, lower=-Inf) =
     _gcp(X, r, (x, m) -> loss(x, m), (x, m) -> LossFunctions.deriv(loss, m, x), lower, (;))
