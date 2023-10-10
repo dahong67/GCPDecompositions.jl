c4fa87515707e197269858eeb01df922dc765cf8
David Hong <hong@udel.edu>
2023-10-10 15:31:08 -0400

    Tweak CPD docstring

--- src/type-cpd.jl
+++ src/type-cpd.jl
@@ -5,10 +5,12 @@
 
 Tensor decomposition type for the canonical polyadic decompositions (CPD)
 of a tensor (i.e., a multi-dimensional array) `A`.
+This is the return type of `gcp(_)`,
+the corresponding tensor decomposition function.
 
-If `F::CPD` is the decomposition object,
-the weights `λ` and factor matrices `U = (U[1],...,U[N])`
-can be obtained via `F.λ` and `F.U`,
+If `M::CPD` is the decomposition object,
+the weights `λ` and the factor matrices `U = (U[1],...,U[N])`
+can be obtained via `M.λ` and `M.U`,
 such that `A = Σ_j λ[j] U[1][:,j] ∘ ⋯ ∘ U[N][:,j]`.
 """
 struct CPD{T,N,Tλ<:AbstractVector{T},TU<:AbstractMatrix{T}}
