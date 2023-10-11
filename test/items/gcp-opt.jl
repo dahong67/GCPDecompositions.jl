de72743772bec9c2fce6ef457ea735f3833aeac9
David Hong <hong@udel.edu>
2023-10-10 21:42:51 -0400

    Add test for PoissonLoss

--- test/items/gcp-opt.jl
+++ test/items/gcp-opt.jl
@@ -19,3 +19,30 @@
         @test maximum(I -> abs(Mh[I] - X[I]), CartesianIndices(X)) <= 1e-5
     end
 end
+
+@testitem "PoissonLoss" begin
+    using Random
+    using Distributions
+
+    @testset "size(X)=$sz, rank(X)=$r" for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
+        Random.seed!(0)
+        M = CPD(fill(10.0, r), rand.(sz, r))
+        X = [rand(Poisson(M[I])) for I in CartesianIndices(size(M))]
+
+        # Compute reference
+        Random.seed!(0)
+        Mr = GCPDecompositions._gcp(
+            X,
+            r,
+            (x, m) -> m - x * log(m + 1e-10),
+            (x, m) -> 1 - x / (m + 1e-10),
+            0.0,
+            (;),
+        )
+
+        # Test
+        Random.seed!(0)
+        Mh = gcp(X, r, PoissonLoss())
+        @test maximum(I -> abs(Mh[I] - Mr[I]), CartesianIndices(X)) <= 1e-5
+    end
+end
