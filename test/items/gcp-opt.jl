2f59b37d77776fc2316ce4b86d117d213ceb57ab
David Hong <hong@udel.edu>
2023-10-12 15:44:33 -0400

    Tweak test case to match a bit better

--- test/items/gcp-opt.jl
+++ test/items/gcp-opt.jl
@@ -98,7 +98,8 @@ end
     @testset "size(X)=$sz, rank(X)=$r" for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
         Random.seed!(0)
         M = CPD(ones(r), rand.(sz, r))
-        X = [rand(Gamma(1.5, M[I])) for I in CartesianIndices(size(M))]
+        k = 1.5
+        X = [rand(Gamma(k, M[I]/k)) for I in CartesianIndices(size(M))]
 
         # Compute reference
         Random.seed!(0)
