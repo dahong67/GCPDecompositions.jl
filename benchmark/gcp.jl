a8b18ec1d14ecd5f8bd8fedb492108b83e7315d1
alexmul1114 <96952477+alexmul1114@users.noreply.github.com>
2023-10-24 20:32:08 -0400

    Add benchmarks for some losses

--- benchmark/gcp.jl
+++ benchmark/gcp.jl
@@ -1,8 +1,8 @@
 using BenchmarkTools
-#const SUITE = BenchmarkGroup()
 
 using Random
 using GCPDecompositions
+using Distributions
 
 const SUITE = BenchmarkGroup()
 
@@ -13,7 +13,30 @@ for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
     Random.seed!(0)
     M = CPD(ones(r), rand.(sz, r))
     X = [M[I] for I in CartesianIndices(size(M))]
+    bench["least-squares-size(X)=$sz, rank(X)=$r"] = @benchmarkable gcp($X, $r, LeastSquaresLoss()) 
+end
 
-    bench["least-squares"] = @benchmarkable gcp(X, r)
-    #bench["least-squares"] = @benchmarkable gcp(X, r, LeastSquaresLoss()) 
+# Benchmark Poisson loss
+for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
+    Random.seed!(0)
+    M = CPD(fill(10.0, r), rand.(sz, r))
+    X = [rand(Poisson(M[I])) for I in CartesianIndices(size(M))]
+    bench["poisson-size(X)=$sz, rank(X)=$r"] = @benchmarkable gcp($X, $r, PoissonLoss())
+end
+
+# Benchmark Gamma loss
+for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
+    Random.seed!(0)
+    M = CPD(ones(r), rand.(sz, r))
+    k = 1.5
+    X = [rand(Gamma(k, M[I]/k)) for I in CartesianIndices(size(M))]
+    bench["gamma-size(X)=$sz, rank(X)=$r"] = @benchmarkable gcp($X, $r, GammaLoss())
+end
+
+# Benchmark BernoulliOdds Loss
+for sz in [(15, 20, 25), (30, 40, 50)], r in 1:2
+    Random.seed!(0)
+    M = CPD(ones(r), rand.(sz, r))
+    X = [rand(Bernoulli(M[I]/(M[I] + 1))) for I in CartesianIndices(size(M))]
+    bench["bernoulliOdds-size(X)=$sz, rank(X)=$r"] = @benchmarkable gcp($X, $r, BernoulliOddsLoss())
 end
