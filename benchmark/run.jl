78847946b7c79376beb2af0c30fd54be61b4cc16
David Hong <hong@udel.edu>
2023-11-14 11:53:47 -0500

    Tidy up run.jl script
    Store in `results` whether doing judgement or not, and use dispatch to
    handle the two cases.
    Write both target and baseline results to files.
    Use BenchmarkConfig whether suites were selected or not.

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -11,59 +11,63 @@
 # > julia benchmark/run.jl --compare eca7cb4
 # 
 # This script produces/overwrites two files:
-# + `benchmark/results.json` : results from the benchmark run
+# + `benchmark/results.json` : results from a benchmark run w/o comparison
+# + `benchmark/results-target.json` : target results from a benchmark run w/ comparison
+# + `benchmark/results-baseline.json` : baseline results from a benchmark run w/ comparison
 # + `benchmark/report.md` : report summarizing the results
 
-## Make sure the benchmark environment is activated
+## Make sure the benchmark environment is activated and load utilities
 import Pkg
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
 include("utils.jl")
 
-## Run benchmarks
-using GCPDecompositions, PkgBenchmark, ArgParse
-
+## Parse command line arguments
+using ArgParse
 settings = ArgParseSettings()
 @add_arg_table settings begin
     "--suite"
         help = "which suite to run benchmarks for" 
         arg_type = String
-        default = "all"
     "--compare"
         help = "git id for previous commit to compare current version against"
         arg_type = String
-        default = "none"
 end
 parsed_args = parse_args(settings)
-compare = parsed_args["compare"]
-suite = parsed_args["suite"]
 
-if (compare == "none")
-    results =
-        suite == "all" ? benchmarkpkg(GCPDecompositions) :
-        benchmarkpkg(
-            GCPDecompositions,
-            BenchmarkConfig(; env = Dict("GCP_BENCHMARK_SUITES" => suite)),
-        )
+## Run benchmarks
+using GCPDecompositions, PkgBenchmark
+
+### Create ENV for BenchmarkConfig
+GCP_ENV =
+    isnothing(parsed_args["suite"]) ? Dict{String,Any}() :
+    Dict("GCP_BENCHMARK_SUITES" => parsed_args["suite"])
+
+### Run benchmark and save
+if isnothing(parsed_args["compare"])
+    results = benchmarkpkg(GCPDecompositions, BenchmarkConfig(; env = GCP_ENV))
     writeresults(joinpath(@__DIR__, "results.json"), results)
 else
-    judgement =
-        suite == "all" ? judge(GCPDecompositions, compare) :
-        judge(
-            GCPDecompositions,
-            BenchmarkConfig(; env = Dict("GCP_BENCHMARK_SUITES" => suite)),
-            BenchmarkConfig(; id=compare, env = Dict("GCP_BENCHMARK_SUITES" => suite)),
-        )
-    results = PkgBenchmark.target_result(judgement)
-    results_baseline = PkgBenchmark.baseline_result(judgement)
-    writeresults(joinpath(@__DIR__, "results.json"), results)
+    results = judge(
+        GCPDecompositions,
+        BenchmarkConfig(; env = GCP_ENV),
+        BenchmarkConfig(; env = GCP_ENV, id = parsed_args["compare"]),
+    )
+    writeresults(
+        joinpath(@__DIR__, "results-target.json"),
+        PkgBenchmark.target_result(results),
+    )
+    writeresults(
+        joinpath(@__DIR__, "results-baseline.json"),
+        PkgBenchmark.baseline_result(results),
+    )
 end
 
 ## Generate report and save
 
-### Create initial/base report
-report = compare != "none" ? sprint(export_markdown, judgement) : sprint(export_markdown, results)
-report *= "\n\n" * (compare != "none" ? GCPBenchmarkUtils.export_mttkrp_sweep(judgement) : GCPBenchmarkUtils.export_mttkrp_sweep(results))
+### Build report
+report = sprint(export_markdown, results)
+report *= "\n\n" * GCPBenchmarkUtils.export_mttkrp_sweep(results)
 
 ### Save report
 write(joinpath(@__DIR__, "report.md"), report)
