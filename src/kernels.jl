a31627633479f847fc7cdd02e437fd51c00dcda4
David Hong <hong@udel.edu>
2024-03-03 22:24:14 -0500

    Factor out and improve dimension checks for khatrirao

--- src/kernels.jl
+++ src/kernels.jl
@@ -129,7 +129,7 @@ Compute the Khatri-Rao product (i.e., the column-wise Kronecker product)
 of the matrices `A1`, `A2`, etc.
 """
 function khatrirao(A::Vararg{T,N}) where {T<:AbstractMatrix,N}
-    I, r = size.(A, 1), (only ∘ unique)(size.(A, 2))
+    I, r = _checked_khatrirao_dims(A...)
     return khatrirao!(similar(A[1], prod(I), r), A...)
 end
 
@@ -140,9 +140,10 @@ Compute the Khatri-Rao product (i.e., the column-wise Kronecker product)
 of the matrices `A1`, `A2`, etc. and store the result in `K`.
 """
 function khatrirao!(K::T, A::Vararg{T,N}) where {T<:AbstractMatrix,N}
-    # Check dimensions
-    Base.require_one_based_indexing(K, A...)
-    I, r = size.(A, 1), (only ∘ unique)(size.(A, 2))
+    I, r = _checked_khatrirao_dims(A...)
+
+    # Check output dimensions
+    Base.require_one_based_indexing(K)
     size(K) == (prod(I), r) || throw(
         DimensionMismatch(
             "Output `K` must have size equal to `(prod(size.(A,1)), size(A[1],2))",
@@ -164,4 +165,21 @@ function khatrirao!(K::T, A::Vararg{T,N}) where {T<:AbstractMatrix,N}
     # Recursive case: N > 2
     n = argmin(n -> I[n] * I[n+1], 1:N-1)
     return khatrirao!(K, A[1:n-1]..., khatrirao(A[n], A[n+1]), A[n+2:end]...)
+end
+
+"""
+    _checked_khatrirao_dims(A1, A2, ...)
+
+Check that `A1`, `A2`, etc. have compatible dimensions for the Khatri-Rao product.
+If so, return a tuple of the number of rows and the shared number of columns.
+If not, throw an error.
+"""
+function _checked_khatrirao_dims(A::Vararg{T,N}) where {T<:AbstractMatrix,N}
+    Base.require_one_based_indexing(A...)
+    allequal(size.(A, 2)) || throw(
+        DimensionMismatch(
+            "Matrices in a Khatri-Rao product must have the same number of columns.",
+        ),
+    )
+    return size.(A, 1), size(A[1], 2)
 end
