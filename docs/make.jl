c3fafa39fba4ffb9c4e2edf237a006d0d6b175fc
David Hong <hong@udel.edu>
2024-03-06 20:19:05 -0500

    Simplify/revise `docs/make.jl`
    Based on guidance in `Documenter.jl` docs (at v1.3.0).

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,12 @@
-using GCPDecompositions
-using Documenter
-
-DocMeta.setdocmeta!(
-    GCPDecompositions,
-    :DocTestSetup,
-    :(using GCPDecompositions);
-    recursive = true,
-)
+using Documenter, GCPDecompositions
 
 makedocs(;
     modules = [GCPDecompositions],
-    authors = "David Hong <hong@udel.edu> and contributors",
-    repo = "https://github.com/dahong67/GCPDecompositions.jl/blob/{commit}{path}#{line}",
     sitename = "GCPDecompositions.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://dahong67.github.io/GCPDecompositions.jl",
-        edit_link = "master",
-        assets = String[],
-    ),
     pages = ["Home" => "index.md"],
+    format = Documenter.HTML(;
+        canonical = "https://dahong67.github.io/GCPDecompositions.jl",
+    ),
 )
 
-deploydocs(; repo = "github.com/dahong67/GCPDecompositions.jl", devbranch = "master")
+deploydocs(; repo = "github.com/dahong67/GCPDecompositions.jl.git")
