18f85733497b3fde433489436fa52fcded360f37
alexmul1114 <96952477+alexmul1114@users.noreply.github.com>
2024-03-14 20:27:31 -0400

    Add buffers

--- src/gcp-algorithms/fastals.jl
+++ src/gcp-algorithms/fastals.jl
@@ -62,17 +62,21 @@ function FastALS_iter!(X, M, order, Jns, Kns, buffers)
     for n in order
         if n == n_star
             if n == 1
-                mul!(M.U[n], reshape(X, (Jns[n], Kns[n])), khatrirao(M.U[reverse(n+1:N)]...))  
+                khatrirao!(buffers.kr_buffer_descending, M.U[reverse(n+1:N)]...)
+                mul!(M.U[n], reshape(X, (Jns[n], Kns[n])), buffers.kr_buffer_descending)  
             else
-                mul!(buffers.descending_buffers[1], reshape(X, (Jns[n], Kns[n])), khatrirao(M.U[reverse(n+1:N)]...))
-                FastALS_mttkrps_helper!(buffers.descending_buffers[1], M.U, n, "right", N, Jns, Kns)
+                khatrirao!(buffers.kr_buffer_descending, M.U[reverse(n+1:N)]...)
+                mul!(buffers.descending_buffers[1], reshape(X, (Jns[n], Kns[n])), buffers.kr_buffer_descending)
+                FastALS_mttkrps_helper!(buffers.descending_buffers[1], M.U, n_star, n, "right", N, Jns, Kns, buffers)
             end
         elseif n == n_star + 1
             if n == N
-                mul!(M.U[n], reshape(X, (Jns[n-1], Kns[n-1]))', khatrirao(M.U[reverse(1:n-1)]...))
+                khatrirao!(buffers.kr_buffer_ascending, M.U[reverse(1:n-1)]...)
+                mul!(M.U[n], reshape(X, (Jns[n-1], Kns[n-1]))', buffers.kr_buffer_ascending)
             else
-                mul!(buffers.ascending_buffers[1], (reshape(X, (Jns[n-1], Kns[n-1])))', khatrirao(M.U[reverse(1:n-1)]...))
-                FastALS_mttkrps_helper!(buffers.ascending_buffers[1], M.U, n, "left", N, Jns, Kns)
+                khatrirao!(buffers.kr_buffer_ascending, M.U[reverse(1:n-1)]...)
+                mul!(buffers.ascending_buffers[1], (reshape(X, (Jns[n-1], Kns[n-1])))', buffers.kr_buffer_ascending)
+                FastALS_mttkrps_helper!(buffers.ascending_buffers[1], M.U, n_star, n, "left", N, Jns, Kns, buffers)
             end  
         elseif n < n_star
             if n == 1
@@ -83,7 +87,7 @@ function FastALS_iter!(X, M, order, Jns, Kns, buffers)
                 for r in 1:R
                     mul!(view(buffers.descending_buffers[n_star-n+1], :, r), reshape(view(buffers.descending_buffers[n_star-n], :, r), (Jns[n], size(X)[n+1])), view(M.U[n+1], :, r))
                 end
-                FastALS_mttkrps_helper!(buffers.descending_buffers[n_star-n+1], M.U, n, "right", N, Jns, Kns)
+                FastALS_mttkrps_helper!(buffers.descending_buffers[n_star-n+1], M.U, n_star, n, "right", N, Jns, Kns, buffers)
             end
         else
             if n == N
@@ -94,31 +98,28 @@ function FastALS_iter!(X, M, order, Jns, Kns, buffers)
                 for r in 1:R
                     mul!(view(buffers.ascending_buffers[n-n_star], :, r), reshape(view(buffers.ascending_buffers[n-n_star-1], :, r), (size(X)[n-1], Kns[n-1]))', view(M.U[n-1], :, r))
                 end
-                FastALS_mttkrps_helper!(buffers.ascending_buffers[n-n_star], M.U, n, "left", N, Jns, Kns)
+                FastALS_mttkrps_helper!(buffers.ascending_buffers[n-n_star], M.U, n_star, n, "left", N, Jns, Kns, buffers)
             end
         end
         # Normalization, update weights
         V = reduce(.*, M.U[i]'M.U[i] for i in setdiff(1:N, n))
-        #M.U[n] = M.U[n] / V
         rdiv!(M.U[n], lu!(V))
         M.λ .= norm.(eachcol(M.U[n]))
         M.U[n] ./= permutedims(M.λ)
-        #M.λ .= norm.(eachcol(M.U[n]))
-        #M.U[n] = M.U[n] ./ permutedims(M.λ)
     end
 end 
 
 
-function FastALS_mttkrps_helper!(Zn, U, n, side, N, Jns, Kns)
+function FastALS_mttkrps_helper!(Zn, U, n_star, n, side, N, Jns, Kns, buffers)
     if side == "right"
-        kr = khatrirao(U[reverse(1:n-1)]...)
+        khatrirao!(buffers.helper_buffers_descending[n_star-n+1], U[reverse(1:n-1)]...)
         for r in 1:size(U[n])[2]
-            U[n][:, r] = reshape(view(Zn, :, r), (Jns[n-1], size(U[n])[1]))' * kr[:, r]
+            U[n][:, r] = reshape(view(Zn, :, r), (Jns[n-1], size(U[n])[1]))' * buffers.helper_buffers_descending[n_star-n+1][:, r]
         end
     elseif side == "left"
-        kr = khatrirao(U[reverse(n+1:N)]...)
+        khatrirao!(buffers.helper_buffers_ascending[n-n_star], U[reverse(n+1:N)]...)
         for r in 1:size(U[n])[2]
-            U[n][:, r] = reshape(view(Zn, :, r), (size(U[n])[1], Kns[n])) * kr[:, r]
+            U[n][:, r] = reshape(view(Zn, :, r), (size(U[n])[1], Kns[n])) * buffers.helper_buffers_ascending[n-n_star][:, r]
         end
     end
 end
@@ -132,11 +133,19 @@ function create_FastALS_buffers(
 
     n_star = order[1]
     r = size(U[1])[2]
+    dims = [size(U[u])[1] for u in 1:length(U)]
 
     # Allocate buffers 
-    # Number of descending buffers = n_star - 1
+    # Buffer for saved products between modes
     descending_buffers = n_star < 2 ? nothing : [similar(U[1], (Jns[n], r)) for n in n_star:-1:2]
-    # Number of ascending buffers = N - n_star - 1
     ascending_buffers = N - n_star - 1 < 1 ? nothing : [similar(U[1], (Kns[n], r)) for n in n_star:N]
-    return(; descending_buffers, ascending_buffers)
+    # Buffers for khatri-rao products
+    kr_buffer_descending = similar(U[1], (Kns[n_star], r))
+    kr_buffer_ascending = similar(U[1], (Jns[n_star], r))
+    # Buffers for khatri-rao product in helper function
+    helper_buffers_descending = n_star < 2 ? nothing : [similar(U[1], (prod(dims[1:n-1]), r)) for n in n_star:-1:2]
+    helper_buffers_ascending = n_star >= N-1 ? nothing : [similar(U[1], (prod(dims[n+1:N]), r)) for n in n_star+1:N-1]
+    return(; descending_buffers, ascending_buffers, 
+            kr_buffer_descending, kr_buffer_ascending,
+            helper_buffers_descending, helper_buffers_ascending)
 end
