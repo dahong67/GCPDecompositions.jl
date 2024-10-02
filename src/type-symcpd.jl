eabed967c09040b6736e91151cbc49895654e3bb
alexmul1114 <96952477+alexmul1114@users.noreply.github.com>
2024-10-02 10:30:39 -0400

    Add empty symcpd type file

---
+++ src/type-symcpd.jl
@@ -0,0 +1,10 @@
+## CP decomposition type
+
+"""
+    SymCPD
+
+Tensor decomposition type for the symmetric canonical polyadic decomposition (Sym-CPD)
+of a tensor (i.e., a multi-dimensional array) `A`.
+"""
+struct SymCPD{}
+end
