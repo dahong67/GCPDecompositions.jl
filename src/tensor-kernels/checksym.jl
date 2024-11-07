122a308597db81516241b9f835a3da47c6bfd07f
alexmul1114 <96952477+alexmul1114@users.noreply.github.com>
2024-11-07 11:53:33 -0500

    move checksym tests to tensor-kernels test file, run formatter

--- src/tensor-kernels/checksym.jl
+++ src/tensor-kernels/checksym.jl
@@ -14,23 +14,16 @@ function checksym(X::AbstractArray, S::NTuple, eps::T = 1e-10) where {T<:Real}
             "Number of modes in X must be same as number of modes given by S",
         ),
     )
-    minimum([S...]) == 1 && maximum([S...]) <= ndims(X) || throw(
-        DimensionMismatch(
-            "Symmetric Groups must be numbered 1,2,... (max N)",
-        ),
-    )
-    eps >= zero(eps) || throw(
-        DomainError(
-          "`eps` must be nonnegative",  
-        ),
-    )
+    minimum([S...]) == 1 && maximum([S...]) <= ndims(X) ||
+        throw(DimensionMismatch("Symmetric Groups must be numbered 1,2,... (max N)"))
+    eps >= zero(eps) || throw(DomainError("`eps` must be nonnegative"))
 
     if !(Tuple(unique(S)) == S)  # Trivial case where number of symmetric groups = number of modes
         for I in CartesianIndices(X)
             for group in unique(S)
                 sym_dims = findall(S .== group)
                 for perm in index_permutations(tuple([Tuple(I)...]...), tuple(sym_dims...))
-                    if abs(X[I] - X[perm...]) > eps  
+                    if abs(X[I] - X[perm...]) > eps
                         sym = false
                         return sym
                     end
@@ -43,19 +36,18 @@ function checksym(X::AbstractArray, S::NTuple, eps::T = 1e-10) where {T<:Real}
 end
 
 function index_permutations(indices::Tuple, sym_dims::Tuple)
-
     values_to_permute = getindex.(Ref(indices), sym_dims)
     permuted_values = permutations(values_to_permute)
 
     result = []
     for perm in permuted_values
-        permuted_indices = [i for i in indices] 
+        permuted_indices = [i for i in indices]
         for (dim, val) in zip(sym_dims, perm)
             permuted_indices[dim] = val
         end
         push!(result, tuple(permuted_indices...))
     end
-    
+
     return result
 end
 
@@ -70,7 +62,7 @@ function permutations(p::Tuple)
         remaining_elements = (p[1:i-1]..., p[i+1:end]...)
         for perm in permutations(remaining_elements)
             push!(result, (fixed_element, perm...))
-        end   
+        end
     end
 
     return result
