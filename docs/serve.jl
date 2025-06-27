efb1771a3d8ddcc59eef9fe04b9e0c642aa9dcda
David Hong <hong@udel.edu>
2025-06-27 19:06:46 -0400

    Update live server script

--- docs/serve.jl
+++ docs/serve.jl
@@ -1,9 +1,21 @@
-using GCPDecompositions
-using LiveServer
+## Serve a live version of the documentation
+
+# Check that Julia version is at least v1.11
+# (needed for the `sources` field used in `docs/Project.toml`)
+VERSION >= v"1.11" || error("Julia version must be at least v1.11")
+
+# Activate and instantiate the `docs` environment
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
 
 # Make list of demos to ignore
-DEMO_DIR = joinpath(pkgdir(GCPDecompositions), "docs", "src", "demos")
+DEMO_DIR = joinpath(@__DIR__, "src", "demos")
 DEMO_FILES = ["$(splitext(f)[1]).md" for f in readdir(DEMO_DIR) if splitext(f)[2] == ".jl"]
 
-# Serve the docs
-servedocs(; launch_browser = true, skip_files = joinpath.(DEMO_DIR, DEMO_FILES))
+# Load packages and serve the docs
+using GCPDecompositions, LiveServer
+Base.exit_on_sigint(false)
+cd(pkgdir(GCPDecompositions)) do
+    servedocs(; skip_files = joinpath.(DEMO_DIR, DEMO_FILES))
+end
