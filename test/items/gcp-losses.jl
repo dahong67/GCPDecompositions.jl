bc06caf361457f1d7d29171ba3f769b6a3b8668f
alexmul1114 <alexmul@udel.edu>
2025-10-02 07:44:57 -0400

    add comments, run formatter

--- test/items/gcp-losses.jl
+++ test/items/gcp-losses.jl
@@ -25,7 +25,8 @@ end
     using InteractiveUtils: subtypes
     using ForwardDiff
     using .GCPLosses: AbstractLoss
-    @testset "loss_func=$loss_type" for loss_type in [GCPLosses.BernoulliLogit,
+    @testset "loss_func=$loss_type" for loss_type in [
+        GCPLosses.BernoulliLogit,
         GCPLosses.BernoulliOdds,
         GCPLosses.BetaDivergence,
         GCPLosses.Gamma,
@@ -35,12 +36,13 @@ end
         GCPLosses.NonnegativeLeastSquares,
         GCPLosses.Poisson,
         GCPLosses.PoissonLog,
-        GCPLosses.Rayleigh]
-        
+        GCPLosses.Rayleigh,
+    ]
+        # Setup
         sz = 10
         r = 2
-        X = rand(sz,sz,sz)
-        M = CPD(ones(r), (rand(sz,r), rand(sz,r), rand(sz,r)))
+        X = rand(sz, sz, sz)
+        M = CPD(ones(r), (rand(sz, r), rand(sz, r), rand(sz, r)))
         if loss_type == GCPLosses.BetaDivergence
             loss_func = loss_type(0.5)
         elseif loss_type == GCPLosses.Huber
@@ -51,7 +53,7 @@ end
             loss_func = loss_type()
         end
 
-        # For autodiff
+        # ForwardDiff requires vectorized objective function
         function form_M(U_λ_vec::Vector{T}) where {T}
             U1 = reshape(U_λ_vec[1:sz*r], (sz, r))
             U2 = reshape(U_λ_vec[sz*r+1:2*sz*r], (sz, r))
@@ -64,17 +66,17 @@ end
         # Check gradients at random init compared to autodiff
         GU = similar.(M.U)
         computed_grad = GCPAlgorithms.gcp_grad_U!(GU, M, X, loss_func)
-        computed_grad_U1, computed_grad_U2, computed_grad_U3 = computed_grad[1], computed_grad[2], computed_grad[3]
-        
+        computed_grad_U1, computed_grad_U2, computed_grad_U3 =
+            computed_grad[1], computed_grad[2], computed_grad[3]
+
         Uλ_vec = vcat(vec(M.U[1]), vec(M.U[2]), vec(M.U[3]), M.λ)
         auto_grad = ForwardDiff.gradient(objective, Uλ_vec)
-        auto_grad_U1 = reshape(auto_grad[1:sz*r], (sz,r))
-        auto_grad_U2 = reshape(auto_grad[sz*r+1:2*sz*r], (sz,r))
-        auto_grad_U3 = reshape(auto_grad[2*sz*r+1:3*sz*r], (sz,r))
-        
-        @test isapprox(computed_grad_U1, auto_grad_U1, rtol=1e-6)
-        @test isapprox(computed_grad_U2, auto_grad_U2, rtol=1e-6)
-        @test isapprox(computed_grad_U3, auto_grad_U3, rtol=1e-6)
+        auto_grad_U1 = reshape(auto_grad[1:sz*r], (sz, r))
+        auto_grad_U2 = reshape(auto_grad[sz*r+1:2*sz*r], (sz, r))
+        auto_grad_U3 = reshape(auto_grad[2*sz*r+1:3*sz*r], (sz, r))
 
+        @test isapprox(computed_grad_U1, auto_grad_U1, rtol = 1e-6)
+        @test isapprox(computed_grad_U2, auto_grad_U2, rtol = 1e-6)
+        @test isapprox(computed_grad_U3, auto_grad_U3, rtol = 1e-6)
     end
 end
