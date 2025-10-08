e2de5cd1d6de9e5cb6c64c7ff19d59232250812a
David Hong <hong@udel.edu>
2025-10-08 10:17:17 -0400

    Reorder losses to match src/gcp-losses.jl

--- test/items/gcp-losses.jl
+++ test/items/gcp-losses.jl
@@ -26,16 +26,16 @@ end
     using ForwardDiff
     using .GCPLosses: AbstractLoss
     @testset "$loss" for (loss, (xvals, mvals)) in [
-        GCPLosses.BernoulliLogit() => (-2:0.5:2, -2:0.5:2),
-        GCPLosses.BernoulliOdds() => (0:0.5:2, 0:0.5:2),
-        GCPLosses.Gamma() => (0:0.5:2, 0.0:0.5:2),
-        GCPLosses.Huber(1) => (-2:0.5:2, -2:0.5:2),
         GCPLosses.LeastSquares() => (-2:0.5:2, -2:0.5:2),
-        GCPLosses.NegativeBinomialOdds(1) => (0:0.5:2, 0:0.5:2),
         GCPLosses.NonnegativeLeastSquares() => (0:0.5:2, 0:0.5:2),
         GCPLosses.Poisson() => (0:0.5:3, 0:0.5:3),
         GCPLosses.PoissonLog() => (-2:0.5:2, -2:0.5:2),
+        GCPLosses.Gamma() => (0:0.5:2, 0.0:0.5:2),
         GCPLosses.Rayleigh() => (0:0.5:2, 0:0.5:2),
+        GCPLosses.BernoulliOdds() => (0:0.5:2, 0:0.5:2),
+        GCPLosses.BernoulliLogit() => (-2:0.5:2, -2:0.5:2),
+        GCPLosses.NegativeBinomialOdds(1) => (0:0.5:2, 0:0.5:2),
+        GCPLosses.Huber(1) => (-2:0.5:2, -2:0.5:2),
         GCPLosses.BetaDivergence(0) => (0:0.5:3, 0.1:0.5:3),
         GCPLosses.BetaDivergence(0.5) => (0:0.5:3, 0.1:0.5:3),
         GCPLosses.BetaDivergence(1) => (0:0.5:3, 0.1:0.5:3),
