cae8044468c1901a56ac443be03b29697511e12f
David Hong <hong@udel.edu>
2025-10-08 10:15:34 -0400

    Combine/simplify list of cases

--- test/items/gcp-losses.jl
+++ test/items/gcp-losses.jl
@@ -25,60 +25,26 @@ end
     using InteractiveUtils: subtypes
     using ForwardDiff
     using .GCPLosses: AbstractLoss
-    @testset "loss_func=$loss_type" for loss_type in [
-        GCPLosses.BernoulliLogit,
-        GCPLosses.BernoulliOdds,
-        GCPLosses.Gamma,
-        GCPLosses.Huber,
-        GCPLosses.LeastSquares,
-        GCPLosses.NegativeBinomialOdds,
-        GCPLosses.NonnegativeLeastSquares,
-        GCPLosses.Poisson,
-        GCPLosses.PoissonLog,
-        GCPLosses.Rayleigh,
+    @testset "$loss" for (loss, (xvals, mvals)) in [
+        GCPLosses.BernoulliLogit() => (-2:0.5:2, -2:0.5:2),
+        GCPLosses.BernoulliOdds() => (0:0.5:2, 0:0.5:2),
+        GCPLosses.Gamma() => (0:0.5:2, 0.0:0.5:2),
+        GCPLosses.Huber(1) => (-2:0.5:2, -2:0.5:2),
+        GCPLosses.LeastSquares() => (-2:0.5:2, -2:0.5:2),
+        GCPLosses.NegativeBinomialOdds(1) => (0:0.5:2, 0:0.5:2),
+        GCPLosses.NonnegativeLeastSquares() => (0:0.5:2, 0:0.5:2),
+        GCPLosses.Poisson() => (0:0.5:3, 0:0.5:3),
+        GCPLosses.PoissonLog() => (-2:0.5:2, -2:0.5:2),
+        GCPLosses.Rayleigh() => (0:0.5:2, 0:0.5:2),
+        GCPLosses.BetaDivergence(0) => (0:0.5:3, 0.1:0.5:3),
+        GCPLosses.BetaDivergence(0.5) => (0:0.5:3, 0.1:0.5:3),
+        GCPLosses.BetaDivergence(1) => (0:0.5:3, 0.1:0.5:3),
     ]
-
-        # Losses with no default parameters
-        loss_params = Dict(GCPLosses.Huber => (1,), GCPLosses.NegativeBinomialOdds => (1,))
-        # Define range of values of (x,m) to test
-        test_vals = Dict(
-            GCPLosses.BetaDivergence => (0.5:0.5:2, 0.5:0.5:2),
-            GCPLosses.BernoulliLogit => (-2:0.5:2, -2:0.5:2),
-            GCPLosses.BernoulliOdds => (0:0.5:2, 0:0.5:2),
-            GCPLosses.Gamma => (0:0.5:2, 0.0:0.5:2),
-            GCPLosses.Huber => (-2:0.5:2, -2:0.5:2),
-            GCPLosses.LeastSquares => (-2:0.5:2, -2:0.5:2),
-            GCPLosses.NegativeBinomialOdds => (0:0.5:2, 0:0.5:2),
-            GCPLosses.NonnegativeLeastSquares => (0:0.5:2, 0:0.5:2),
-            GCPLosses.Poisson => (0:0.5:3, 0:0.5:3),
-            GCPLosses.PoissonLog => (-2:0.5:2, -2:0.5:2),
-            GCPLosses.Rayleigh => (0:0.5:2, 0:0.5:2),
-        )
-        loss_func =
-            haskey(loss_params, loss_type) ? loss_type(loss_params[loss_type]...) :
-            loss_type()
-
-        for x in test_vals[loss_type][1]
-            for m in test_vals[loss_type][2]
-                auto_diff_func(m) = GCPLosses.value(loss_func, x, m)
-                computed_diff = GCPLosses.deriv(loss_func, x, m)
-                auto_diff = ForwardDiff.derivative(auto_diff_func, m)
-                @test isapprox(computed_diff, auto_diff, rtol = 1e-6)
-            end
-        end
-    end
-
-    @testset "loss_func=GCPDecompositions.GCPLosses.BetaDivergence, beta=$β" for β in
-                                                                                 [0, 0.5, 1]
-        loss_func = GCPLosses.BetaDivergence(β)
-        test_vals = (0:0.5:3, 0.1:0.5:3)
-        for x in test_vals[1]
-            for m in test_vals[2]
-                auto_diff_func(m) = GCPLosses.value(loss_func, x, m)
-                computed_diff = GCPLosses.deriv(loss_func, x, m)
-                auto_diff = ForwardDiff.derivative(auto_diff_func, m)
-                @test isapprox(computed_diff, auto_diff, rtol = 1e-6)
-            end
+        for x in xvals, m in mvals
+            auto_diff_func(m) = GCPLosses.value(loss, x, m)
+            computed_diff = GCPLosses.deriv(loss, x, m)
+            auto_diff = ForwardDiff.derivative(auto_diff_func, m)
+            @test isapprox(computed_diff, auto_diff, rtol = 1e-6)
         end
     end
 end
