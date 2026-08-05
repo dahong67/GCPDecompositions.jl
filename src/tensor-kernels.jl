c57e0f95a2cd935b85cd46c4d34414738e606d30
alexmul1114 <alexmul@udel.edu>
2026-08-05 14:23:10 -0400

    remove old functions

--- src/tensor-kernels.jl
+++ src/tensor-kernels.jl
@@ -13,8 +13,8 @@ using Base.Cartesian: @nloops, @ntuple
 using StaticArrays: MVector
 #using SparseTensors: AbstractSparseTensor, numstored, storedindices, storedvalues
 export create_mttkrp_buffer, mttkrp, mttkrp!, mttkrps, mttkrps!, khatrirao, khatrirao!, checksym, sparse_mttkrp!, sparse_mttkrps!, 
-    symmetric_mttkrp!, create_symmetric_mttkrp_buffer, symmetric_kr!, symmetric_self_kr!, symmetric_kr_partial_products!
-export symmetric_mttkrp!, symmetric_kr!, symmetric_kr_unweighted!
+    symmetric_mttkrp!, create_symmetric_mttkrp_buffer, symmetric_kr!, symmetric_self_kr!, symmetric_kr_unweighted!, symmetric_self_kr_unweighted!
+    
 include("tensor-kernels/khatrirao.jl")
 include("tensor-kernels/mttkrp.jl")
 include("tensor-kernels/mttkrps.jl")
